1ba81190eb6e4bb84423953a674bcc6a50384f32
x8n1620 <52535196+x8n1620@users.noreply.github.com>
2019-07-04 15:29:38 +0200

    Add files via upload

--- inc/db.php
+++ inc/db.php
@@ -1,1 +1,17 @@
-1
+<?php
+$db_host = "localhost"; // Host-Adresse
+$db_name = "bluebattery"; // Datenbankname
+$db_user = "<Benutzername>"; // Benutzername -> bitte anpassen!
+$db_pwd = "<Password>"; // Passwort -> bitte anpassen!
+
+error_reporting(E_ALL);
+//error_reporting(0);
+$db = new mysqli("$db_host", "$db_user", "$db_pwd", "$db_name");
+
+
+print_r ($db->connect_error);
+
+if ($db->connect_errno) {
+    die('Es ist ein Problem aufgetreten');
+}
+?>
