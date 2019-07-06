9381d68ec22752da740cb307948aab967882629c
x8n1620 <manuel@fleder.de>
2019-07-06 20:39:05 +0200

    Version 2019-07-06

--- history_import.php
+++ history_import.php
@@ -10,7 +10,7 @@ if(isset($_POST["Import"])) {
 			   $sql = "INSERT into history (Geraeteadresse,Geraetename,Datum,Solarenergie_Wh,Solarladung_mAh,Solarleistung_W,max_Solarstrom_mA,
 					Ladezeit_Minuten,Batterieladestand_mAh,maximaler_Batterieladestand_mAh,minimaler_Batterieladestand_mAh,
 					maximaler_Batteriestrom_mA,minimaler_Batteriestrom_mA,max_Batteriespannung_mV,min_Batteriespannung_mV,
-					max_Temperatur,min_Temp,Externe_Ladung_mAh,Entnahme_mAh,Ladung_mAh,Verbrauch_mAh)
+					max_Temp,min_Temp,Externe_Ladung_mAh,Entnahme_mAh,Ladung_mAh,Verbrauch_mAh)
 				values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."','".$getData[6]."',
 					'".$getData[7]."','".$getData[8]."','".$getData[9]."','".$getData[10]."','".$getData[11]."','".$getData[12]."','".$getData[13]."',
 					'".$getData[14]."','".$getData[15]."','".$getData[16]."','".$getData[17]."','".$getData[18]."','".$getData[19]."','".$getData[20]."')";
